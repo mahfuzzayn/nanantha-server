c503ba062afa259e5bb8166a3635981c58756b23
mahfuzzayn <mahfuzzayn8gmail.com>
2025-02-09 03:03:09 +0600

    Third Final Commit with Update on Cloudinary Uploading System

--- src/app/utils/sendImageToCloudinary.ts
+++ src/app/utils/sendImageToCloudinary.ts
@@ -1,7 +1,7 @@
 import { UploadApiResponse, v2 as cloudinary } from 'cloudinary'
-import fs from 'fs'
 import multer from 'multer'
 import config from '../config'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
 
 cloudinary.config({
     cloud_name: config.cloudinary_cloud_name,
@@ -22,27 +22,13 @@ export const sendImageToCloudinary = (
                     reject(error)
                 }
                 resolve(result as UploadApiResponse)
-                // delete a file asynchronously
-                fs.unlink(path, err => {
-                    if (err) {
-                        console.log(err)
-                    } else {
-                        console.log('File is deleted.')
-                    }
-                })
             },
         )
     })
 }
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, process.cwd() + '/uploads/')
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-        cb(null, file.fieldname + '-' + uniqueSuffix)
-    },
+const storage = new CloudinaryStorage({
+    cloudinary,
 })
 
 export const upload = multer({ storage: storage })
