f11ee6e475335eaa94c3777ff28095442d5eca86
mahfuzzayn <mahfuzzayn8gmail.com>
2025-02-09 03:31:37 +0600

    Fourth Final Commit with QueryBuilder Update

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -81,7 +81,7 @@ class QueryBuilder<T> {
 
     paginate() {
         const page = Number(this?.query?.page) || 1
-        const limit = Number(this?.query?.limit) || 10
+        const limit = Number(this?.query?.limit) || 999999
         const skip = (page - 1) * limit
 
         this.modelQuery = this.modelQuery.skip(skip).limit(limit)
@@ -100,7 +100,7 @@ class QueryBuilder<T> {
         const totalQueries = this.modelQuery.getFilter()
         const total = await this.modelQuery.model.countDocuments(totalQueries)
         const page = Number(this?.query?.page) || 1
-        const limit = Number(this?.query?.limit) || 10
+        const limit = Number(this?.query?.limit) || 999999
         const totalPage = Math.ceil(total / limit)
 
         return {
